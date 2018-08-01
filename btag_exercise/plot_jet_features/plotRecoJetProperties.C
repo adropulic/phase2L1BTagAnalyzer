a7aa69b16ad7a181c8be2eff54d3903c0d033e21
skkwan <skkwan@caltech.edu>
2018-08-01 11:46:52 +0200

    fixed casing in test-Analyzer file names, updating plotting script for TTbar samples

--- btag_exercise/plot_jet_features/plotRecoJetProperties.C
+++ btag_exercise/plot_jet_features/plotRecoJetProperties.C
@@ -15,7 +15,7 @@ void plotRecoJetProperties() {
 	/// Load data and initialize
 	////////////////////////////
 
-	const char *fname = "/afs/cern.ch/work/s/skkwan/public/triggerDevel/CMSSW_10_1_5/src/L1Trigger/phase2L1BTagAnalyzer/test/analyzer.root";
+	const char *fname = "/afs/cern.ch/work/s/skkwan/public/triggerDevel/CMSSW_10_1_5/src/L1Trigger/phase2L1BTagAnalyzer/test/analyzer_ZTTbar.root";
 	// Load file with checks on whether it exists
 	TFile *input = 0;
 	if (!gSystem->AccessPathName(fname)) {
@@ -53,32 +53,55 @@ void plotRecoJetProperties() {
 	// ranges in TBrowser / interactively in ROOT as a first pass.
 	//////////////////////////////////////////////////////////////////////
 
-	// Global options (see TStyle documentation for all options)
+	// GStyle options (see TStyle documentation for all options)
 	gStyle->SetOptStat(0); 
+	TPaveLabel *title = new TPaveLabel(0.11, 0.95, 0.35, 0.99, "Z to TT samples", "br");
+	gStyle->SetLabelSize(0.08, "x");  // size is expressed as a percent of the pad height, defaults to 0.04 and x-axis
+	gStyle->SetLabelSize(0.08, "y");  // y-axis labels need to be smaller
+	gStyle->SetTitleSize(0.24); 	  // size defaults to 0.02 and axis defaults to x
+
+	// Set opacity for signal and background histograms
+	Float_t sigAlpha = 0.7;
+	Float_t bkgAlpha = 0.7;
 
 	// Signal histograms
 	gStyle->SetHistLineColor(kRed); 
-	gStyle->SetHistFillColor(kRed);
 	gStyle->SetHistFillStyle(3001);	 
-	TH1D *sig_recoPt = new TH1D("sig_recoPt", "sig_recoPt", 100, 0, 175);
-	TH1D *sig_recoEta = new TH1D("sig_recoEta", "sig_recoEta", 100, -2.7, 2.7);
-	TH1D *sig_recoPhi = new TH1D("sig_recoPhi", "sig_recoPhi", 100, -3.1, 3.1);
-	TH1D *sig_recoTk1IP = new TH1D("sig_recoTk1IP", "sig_recoTk1IP", 70, -0.1, 0.1);
-	TH1D *sig_recoTk2IP = new TH1D("sig_recoTk2IP", "sig_recoTk2IP", 70, -0.5, 0.5);
-	TH1D *sig_recoTk3IP = new TH1D("sig_recoTk3IP", "sig_recoTk3IP", 700, -0.5, 0.5);
-	TH1D *sig_recoTk4IP = new TH1D("sig_recoTk4IP", "sig_recoTk4IP", 70, -0.5, 0.5);
+	TH1D *sig_recoPt = new TH1D("sig_recoPt", "recoPt", 100, 0, 175);
+	TH1D *sig_recoEta = new TH1D("sig_recoEta", "recoEta", 100, -2.7, 2.7);
+	TH1D *sig_recoPhi = new TH1D("sig_recoPhi", "recoPhi", 100, -3.1, 3.1);
+	TH1D *sig_recoTk1IP = new TH1D("sig_recoTk1IP", "recoTk1IP", 70, -0.1, 0.1);
+	TH1D *sig_recoTk2IP = new TH1D("sig_recoTk2IP", "recoTk2IP", 70, -0.5, 0.5);
+	TH1D *sig_recoTk3IP = new TH1D("sig_recoTk3IP", "recoTk3IP", 700, -0.5, 0.5);
+	TH1D *sig_recoTk4IP = new TH1D("sig_recoTk4IP", "recoTk4IP", 70, -0.5, 0.5);
+	// Need to set color opacity separately for each histogram:
+	sig_recoPt->SetFillColorAlpha(kRed, sigAlpha);
+	sig_recoEta->SetFillColorAlpha(kRed, sigAlpha);
+	sig_recoPhi->SetFillColorAlpha(kRed, sigAlpha);
+	sig_recoTk1IP->SetFillColorAlpha(kRed, sigAlpha);
+	sig_recoTk2IP->SetFillColorAlpha(kRed, sigAlpha);
+	sig_recoTk3IP->SetFillColorAlpha(kRed, sigAlpha);
+	sig_recoTk4IP->SetFillColorAlpha(kRed, sigAlpha);
 		
 	// Likewise, background histograms
 	gStyle->SetHistLineColor(kBlue); 
-	gStyle->SetHistFillColor(kBlue);
 	gStyle->SetHistFillStyle(3001);
-	TH1D *bkg_recoPt = new TH1D("bkg_recoPt", "bkg recoPt", 100, 0, 175);
-	TH1D *bkg_recoEta = new TH1D("bkg_recoEta", "bkg_recoEta", 100, -2.7, 2.7);
-	TH1D *bkg_recoPhi = new TH1D("bkg_recoPhi", "bkg_recoPhi", 100, -3.1, 3.1);
-	TH1D *bkg_recoTk1IP = new TH1D("bkg_recoTk1IP", "sig_recoTk1IP", 70, -0.1, 0.1);
-	TH1D *bkg_recoTk2IP = new TH1D("bkg_recoTk2IP", "bkg_recoTk2IP", 70, -0.5, 0.5);
-	TH1D *bkg_recoTk3IP = new TH1D("bkg_recoTk3IP", "bkg_recoTk3IP", 70, -0.5, 0.5);
-	TH1D *bkg_recoTk4IP = new TH1D("bkg_recoTk4IP", "bkg_recoTk4IP", 70, -0.5, 0.5);
+	TH1D *bkg_recoPt = new TH1D("bkg_recoPt", "recoPt", 100, 0, 175);
+	TH1D *bkg_recoEta = new TH1D("bkg_recoEta", "recoEta", 100, -2.7, 2.7);
+	TH1D *bkg_recoPhi = new TH1D("bkg_recoPhi", "recoPhi", 100, -3.1, 3.1);
+	TH1D *bkg_recoTk1IP = new TH1D("bkg_recoTk1IP", "recoTk1IP", 70, -0.1, 0.1);
+	TH1D *bkg_recoTk2IP = new TH1D("bkg_recoTk2IP", "recoTk2IP", 70, -0.5, 0.5);
+	TH1D *bkg_recoTk3IP = new TH1D("bkg_recoTk3IP", "recoTk3IP", 70, -0.5, 0.5);
+	TH1D *bkg_recoTk4IP = new TH1D("bkg_recoTk4IP", "recoTk4IP", 70, -0.5, 0.5);
+	// Likewise, need to set color opacity separately for each histogram:
+	bkg_recoPt->SetFillColorAlpha(kBlue, bkgAlpha);
+	bkg_recoEta->SetFillColorAlpha(kBlue, bkgAlpha);
+	bkg_recoPhi->SetFillColorAlpha(kBlue, bkgAlpha);
+	bkg_recoTk1IP->SetFillColorAlpha(kBlue, bkgAlpha);
+	bkg_recoTk2IP->SetFillColorAlpha(kBlue, bkgAlpha);
+	bkg_recoTk3IP->SetFillColorAlpha(kBlue, bkgAlpha);
+	bkg_recoTk4IP->SetFillColorAlpha(kBlue, bkgAlpha);
+
 
 	// Loop through jets and use hadronFlavor to distinguish signal and background
 	Int_t i;
@@ -111,36 +134,41 @@ void plotRecoJetProperties() {
 	TCanvas* sb_canvas = new TCanvas("sb_canvas", "Reconstructed jet variables");
 	sb_canvas->Divide(2, 4);
 	
+	sb_canvas->cd(1); 
+	bkg_recoPt->DrawNormalized();  sig_recoPt->DrawNormalized("same");
+	//	sb_canvas->SetTitle("recoPt");
 
-	sb_canvas->cd(1); sb_canvas->SetTitle("recoPt");
-	sig_recoPt->DrawNormalized();  bkg_recoPt->DrawNormalized("same");
+	sb_canvas->cd(2); 
+	bkg_recoEta->DrawNormalized(); sig_recoEta->DrawNormalized("same"); 
+	//	sb_canvas->SetTitle("reco Eta");
 
-	sb_canvas->cd(2); sb_canvas->SetTitle("reco Eta");
-	sig_recoEta->DrawNormalized(); bkg_recoEta->DrawNormalized("same"); 
+	sb_canvas->cd(3); 
+	bkg_recoPhi->DrawNormalized(); sig_recoPhi->DrawNormalized("same"); 
+	//	sb_canvas->SetTitle("reco Phi");
 
-	sb_canvas->cd(3); sb_canvas->SetTitle("reco Phi");
-	sig_recoPhi->DrawNormalized(); bkg_recoPhi->DrawNormalized("same"); 
+	sb_canvas->cd(4); 
+	bkg_recoTk1IP->DrawNormalized(); sig_recoTk1IP->DrawNormalized("same"); 
+	//	sb_canvas->SetTitle("recoTk1IP");
 
-	sb_canvas->cd(4); sb_canvas->SetTitle("recoTk1IP");
-	sig_recoTk1IP->DrawNormalized(); bkg_recoTk1IP->DrawNormalized("same"); 
+	sb_canvas->cd(5);
+	bkg_recoTk2IP->DrawNormalized(); sig_recoTk2IP->DrawNormalized("same"); 
+	//	sb_canvas->SetTitle("recoTk2IP");
 
-	sb_canvas->cd(5); sb_canvas->SetTitle("recoTk2IP");
-	sig_recoTk2IP->DrawNormalized(); bkg_recoTk2IP->DrawNormalized("same"); 
+	sb_canvas->cd(6); 
+	bkg_recoTk3IP->DrawNormalized(); sig_recoTk3IP->DrawNormalized("same"); 
+	//	sb_canvas->SetTitle("recoTk3IP");
 
-	sb_canvas->cd(6); sb_canvas->SetTitle("recoTk3IP");
-	sig_recoTk3IP->DrawNormalized(); bkg_recoTk3IP->DrawNormalized("same"); 
+	sb_canvas->cd(7); 
+	bkg_recoTk4IP->DrawNormalized(); sig_recoTk4IP->DrawNormalized("same"); 
+	//	sb_canvas->SetTitle("recoTk4IP");
 
-	sb_canvas->cd(7); sb_canvas->SetTitle("recoTk4IP");
-	sig_recoTk4IP->DrawNormalized(); bkg_recoTk4IP->DrawNormalized("same"); 
-
-	// Create legend
+	// Create legend and title (defined above)
 	sb_canvas->cd(8);
-	TLegend *legend = new TLegend(0.2, 0.2, .6, .6);
+	TLegend *legend = new TLegend(0.2, 0.2, 0.8, 0.8, "Z to TTbar events");
 	legend->AddEntry(sig_recoPt, "Signal (hadronFlavor == 5)");
 	legend->AddEntry(bkg_recoPt, "Background (hadronFlavor != 5)");
 	legend->Draw();
-
-	sb_canvas->SaveAs("output_plotRecoJetProperties.pdf");
-
+	sb_canvas->SaveAs("output_ZTTbar.pdf");
+	// Close file
 	input->Close();
 }
