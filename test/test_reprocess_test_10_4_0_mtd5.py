63976c14f4a971f22746afbcdbf554b712d4dea6
skkwan <kystephkwan2@gmail.com>
2019-04-24 22:17:40 +0200

    Multiple secondaryFileNames

--- test/test_reprocess_test_10_4_0_mtd5.py
+++ test/test_reprocess_test_10_4_0_mtd5.py
@@ -36,8 +36,8 @@ process.source = cms.Source("PoolSource",
                                                                        '/store/mc/PhaseIIMTDTDRAutumn18DR/DYToLL_M-50_14TeV_TuneCP5_pythia8/FEVT/PU200_103X_upgrade2023_realistic_v2-v2/910000/97A38B8C-B7B7-C041-AC10-B2794F5B81CB.root',
                                                                        '/store/mc/PhaseIIMTDTDRAutumn18DR/DYToLL_M-50_14TeV_TuneCP5_pythia8/FEVT/PU200_103X_upgrade2023_realistic_v2-v2/910000/9D485410-BBF7-D841-9048-E273729D9E24',
                                                                        '/store/mc/PhaseIIMTDTDRAutumn18DR/DYToLL_M-50_14TeV_TuneCP5_pythia8/FEVT/PU200_103X_upgrade2023_realistic_v2-v2/910000/E4590EF0-336D-CB48-B5D8-2349E8573FCF.root',
-                                                                       '/store/mc/PhaseIIMTDTDRAutumn18DR/DYToLL_M-50_14TeV_TuneCP5_pythia8/FEVT/PU200_103X_upgrade2023_realistic_v2-v2/910000/F37B1F94-AD89-DF40-8D40-3EE87E8339A1.root')
-
+                                                                       '/store/mc/PhaseIIMTDTDRAutumn18DR/DYToLL_M-50_14TeV_TuneCP5_pythia8/FEVT/PU200_103X_upgrade2023_realistic_v2-v2/910000/F37B1F94-AD89-DF40-8D40-3EE87E8339A1.root'
+)
 )
 
 process.source.lumisToProcess = cms.untracked.VLuminosityBlockRange("1:337")
