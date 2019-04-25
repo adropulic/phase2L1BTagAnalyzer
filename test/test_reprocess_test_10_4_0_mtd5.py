29edd45cb545fd5516093ffc7cc803843e128ed9
skkwan <kystephkwan2@gmail.com>
2019-04-25 19:09:17 +0200

    process = cms.Process('REPR') in analyzer py

--- test/test_reprocess_test_10_4_0_mtd5.py
+++ test/test_reprocess_test_10_4_0_mtd5.py
@@ -3,12 +3,15 @@
 # Revision: 1.19 
 # Source: /local/reps/CMSSW/CMSSW/Configuration/Applications/python/ConfigBuilder.py,v 
 # with command line options: repr --processName=REPR --python_filename=reprocess_test_10_5_0_pre1.py --no_exec -s L1 --datatier GEN-SIM-DIGI-RAW -n 2 --era Phase2 --eventcontent FEVTDEBUGHLT --filein root://cms-xrd-global.cern.ch//store/mc/PhaseIIMTDTDRAutumn18DR/DYToLL_M-50_14TeV_pythia8/FEVT/PU200_pilot_103X_upgrade2023_realistic_v2_ext4-v1/280000/FF5C31D5-D96E-5E48-B97F-61A0E00DF5C4.root --conditions 103X_upgrade2023_realistic_v2 --beamspot HLLHC14TeV --geometry Extended2023D28 --fileout file:step2_2ev_reprocess_slim.root
+
 import FWCore.ParameterSet.Config as cms
 
 from Configuration.StandardSequences.Eras import eras
 
-process = cms.Process('REPR',eras.Phase2C4_trigger)
+#process = cms.Process('REPR',eras.Phase2C4_trigger)
 #process = cms.Process('REPR',eras.Phase2C4_timing_layer_bar)
+
+process = cms.Process('REPR')
 
 # import of standard configurations
 process.load('Configuration.StandardSequences.Services_cff')
