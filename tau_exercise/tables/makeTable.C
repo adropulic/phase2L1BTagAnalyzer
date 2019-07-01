1eaac17bd2972bffe8b209928d17347602fd06c8
skkwan <kystephkwan2@gmail.com>
2019-07-01 16:15:01 +0200

    printTable now does not call global variables

--- tau_exercise/tables/makeTable.C
+++ tau_exercise/tables/makeTable.C
@@ -1,7 +1,7 @@
-/*********************************************************/
-/* makeTable.C                                           */
-/* Author: Stephanie Kwan                                */
-/*********************************************************/
+/**************************************************************/
+/* makeTable.C                                                */
+/* Author: Stephanie Kwan                                     */
+/**************************************************************/
 
 /* Usage:
    root .x makeTable.C */
@@ -9,33 +9,43 @@
 #include <iostream>
 #include <array>
 
-/* Global variables */
-int numVariables = 2;
+/**************************************************************/
 
-int dBins[] = {4, 4};
-double dL1Pt[] = {0.0, 20.0, 100.0, 500.0};
-double dL1Eta[] = {-2.25, -0.75, 0.75, 2.25};
-double *pBins[] = {dL1Pt, dL1Eta};
+/* Number of variables */
+int NumVars = 2;
 
-/* Print the contents of a 2D array. */
-void printTable(double *array[], int numVars)
+/* Number of bins for each variable */
+int NumBins[] = {4, 4};
+
+/* Declare bins for each variable (must be in the same order
+   as iNumBins). */ 
+double BinsL1Pt[] = {0.0, 20.0, 100.0, 500.0};
+double BinsL1Eta[] = {-2.25, -0.75, 0.75, 2.25};
+double *Bins[] = {BinsL1Pt, BinsL1Eta};
+
+
+/**************************************************************/
+
+/* Print the contents of pdArray, which is a list of iNumVars
+   pointers, which point to lists of doubles. These lists can
+   have variable lengths, which are specified in piBins. */
+
+void printTable(double *pdArray[], int iNumVars, int piBins[])
 {
-
-  for (int i = 0; i < numVars; i++)
+  for (int i = 0; i < iNumVars; i++)
     {
-      int width = dBins[i];
-
-      for (int j = 0; j < width; j++)
+      for (int j = 0; j < piBins[i]; j++)
 	{
-	  std::cout << array[i][j] << ' ';
+	  std::cout << pdArray[i][j] << ' ';
 	}
       std::cout << std::endl;
     }
-  
 }
+
+/**********************************************************/
 
 /* Main function. */
 void makeTable(void)
 {
-  printTable(pBins, numVariables);
+  printTable(Bins, NumVars, NumBins);
 }
