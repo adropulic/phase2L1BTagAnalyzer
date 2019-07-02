bcf3e32d1b3a705f4248c5e835c5e5e77d69be75
skkwan <kystephkwan2@gmail.com>
2019-07-02 11:27:12 +0200

    working on using tables not ttrees

--- tau_exercise/tables/makeTable.C
+++ tau_exercise/tables/makeTable.C
@@ -3,12 +3,21 @@
 /* Author: Stephanie Kwan                                     */
 /**************************************************************/
 
-/* Usage:
+/* Usage: specify the variables in the makeTable function.
    root .x makeTable.C */
-   
+
+#include <vector>
+
 #include <iostream>
 #include <array>
 #include <stdio.h>
+
+#include "TROOT.h"
+#include "TFile.h"
+#include "TTree.h"
+#include "TCanvas.h"
+#include "TFrame.h"
+#include "TH1F.h"
 
 /**************************************************************/
 
@@ -18,11 +27,11 @@ int NumVars = 2;
 /* Number of bins for each variable */
 int NumBins[] = {4, 4};
 
-/* Declare bins for each variable (must be in the same order
+/* Declare values for each variable (must be in the same order
    as iNumBins). */ 
-double BinsL1Pt[] = {0.0, 20.0, 100.0, 500.0};
-double BinsL1Eta[] = {-2.25, -0.75, 0.75, 2.25};
-double *Bins[] = {BinsL1Pt, BinsL1Eta};
+double ValsL1Pt[] = {10., 35., 75., 300.};
+double ValsL1Eta[] = {-2.25, -0.75, 0.75, 2.25};
+double *Vals[] = {ValsL1Pt, ValsL1Eta};
 
 
 /**************************************************************/
@@ -30,7 +39,7 @@ double *Bins[] = {BinsL1Pt, BinsL1Eta};
 /* Print the contents of pdArray, which is a list of iNumVars
    pointers, which point to lists of doubles. These lists can
    have variable lengths, which are specified in piBins. */
-
+/*
 void printTable(double *pdArray[], int iNumVars, int piBins[])
 {
   for (int i = 0; i < iNumVars; i++)
@@ -42,7 +51,7 @@ void printTable(double *pdArray[], int iNumVars, int piBins[])
       std::cout << std::endl;
     }
 }
-
+*/
 /**************************************************************/
 
 /* Prepare 2D array of doubles, with an extra column for the
@@ -50,50 +59,90 @@ void printTable(double *pdArray[], int iNumVars, int piBins[])
    piBins is a list of ints that are the (variable ok) number 
    of bins. */
 
-/* double**   */
-double** create2DArray(int numVars, int piBins[])
+double** fillTableWithPermutedValues(int numVars,
+				     int piBins[])
 {
-  /* The 2D array of doubles. */
-  double **table;
+  assert(numVars > 0);
+  assert(piBins != NULL);
 
-  /* Total number of rows. */
-  int nRows = 1;
-
-  /* Index to loop over rows. */
-  int iRow;
-
-  /* Calculate total number of rows. */
-  for (int i = 0; i < numVars; i++)
+  /* Total number of rows. Initialize to 1 and calculate it. */
+  Int_t nRows = 1;
+  for (Int_t i = 0; i < numVars; i++)
       nRows *= piBins[i];
 
-  /* Allocate memory for the new table. */
-  table = new double*[nRows];
-  for (int iRow = 0; iRow < nRows; iRow++)
-      table[iRow] = new double[numVars + 1];
+  /* Allocate memory for the new table, adding an extra 
+     column for the discriminant value. */
+  double** table = new double*[nRows];
+  for (Int_t r = 0; r < nRows; r++)
+    table[r] = new double[numVars + 1];
 
-  /* For each variable, */
-  for (int i = 0; i < numVars; i++)
+  /* Fill the table with permutations of the variables' possible
+     values. */
+  
+  /* For each variable: */
+  for (Int_t iVar = 0; iVar < numVars; iVar++)
     {
       /* Start at the top of a column: */
-      iRow = 0;
+      Int_t iRow = 0;
+      
+      /* Calculate the product of the remaining variables' number
+	 of bins. This is the "block size" that we need to fill
+         in one step. */
+      Int_t blockSize = 1;
+      for (Int_t i = iVar; i < numVars; i++)
+	blockSize *= piBins[i];
+      
+      while (iRow < nRows)
+	{
+	  /* Index iBin counts which bin we are on. */
+	  for (Int_t iBin = 0; iBin < piBins[iBin]; iBin++)
+	    {
+	      /* Index r counts which row we are on. */
+	      for (Int_t r = iRow; r < (iRow + blockSize); r++)
+		{
+		  /* Fill the entry. */
+		  printf("%d\n", r);
+		  //	  table[r][iVar] = Vals[iVar][iBin];
+		}
+	      iRow += blockSize;
+	    }
+	}
     }
 
 
   return table;
 }
 
+/**************************************************************/
+
+
 
 
 /**************************************************************/
 
 /* Main function. */
-void makeTable(void)
+int makeTable(void)
 {
-  double **table = create2DArray(NumVars, NumBins);
+  /* Create the file, the tree, and the branches. */
+  /*  TFile f("table.root", "RECREATE");
+  TTree t1("t1", "a Tree with five variables");
 
-  /* Fill the array. */
+  Float_t l1Pt, l1Eta;
+  Float_T track1ChiSquared, tauL1StripPt, l1DecayMode;
+  Double_t discr;
 
-  //  printTable(Bins, NumVars, NumBins);
+  t1.Branch("l1Pt", &l1Pt, "l1Pt/F");
+  t1.Branch("l1Eta", &l1Eta, "l1Eta/F");
+  t1.Branch("track1ChiSquared", &track1ChiSquared,
+	    "track1ChiSquared/F");
+  t1.Branch("tauL1StripPt", &tauL1StripPt, "tauL1StripPt/F");
+  t1.Branch("discr", &discr, "discr/D");
+  */
+  // Fill the tree.
+  
+  double **table = fillTableWithPermutedValues(NumVars, NumBins);
 
   delete(table);
+  
+  return 0;
 }
